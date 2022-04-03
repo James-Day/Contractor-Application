7766b965ce12eb96139649e2c0e556e1877b3d54
James-Day <85190098+James-Day@users.noreply.github.com>
2022-04-03 00:56:18 -0700

    RecruiterReadDto
    Dto for reading a recruiters profile

---
+++ contractor_web_api/Dtos/RecruiterReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace contractor_web_api.Dtos
+{
+    public class RecruiterReadDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Company { get; set; }
+    }
+}
